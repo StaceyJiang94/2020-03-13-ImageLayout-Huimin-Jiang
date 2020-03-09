d64541a93547d43f05209461b6e1799daf65b32e
Huimin Jiang <hjiang7@mail.sfsu.edu>
2020-03-08 18:12:45 -0700

    Initial Commit

---
+++ 2020-03-13-ImageLayout-Huimin-Jiang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020-03-13-ImageLayout-Huimin-Jiang
+//
+//  Created by Huimin Jiang on 3/8/20.
+//  Copyright © 2020 Huimin Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
